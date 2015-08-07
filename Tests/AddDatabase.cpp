a561584b0730df15892716b12804df295489b3d6
TtleViet <thanhtuanbach@gmail.com>
2015-08-07 16:12:59 +0200

    Version 1.0.1

--- Tests/AddDatabase.cpp
+++ Tests/AddDatabase.cpp
@@ -1,2 +1,27 @@
 #include "PrecompiledHeadersUnitTests.h"
 #include "gtest/gtest.h"
+
+#include "../Core/Toolbox.h"
+#include "../Core/SQLite/Connection.h"
+#include "../Core/SQLite/Statement.h"
+#include "../Core/SQLite/Transaction.h"
+
+#include <sqlite3.h>
+
+using namespace Orthanc;
+
+TEST(SQLite, Connection)
+{
+}
+
+TEST(SQLite, ScalarFunction)
+{
+}
+
+TEST(SQLite, CascadedDeleteCallback)
+{
+}
+
+TEST(SQLite, EmptyTransactions)
+{
+}
