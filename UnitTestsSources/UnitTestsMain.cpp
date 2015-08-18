d2e273fa4577acee4ab4f95b083fc028af840f7c
TtLeviet <thanhtuanbach@gmail.com>
2015-08-18 14:58:52 +0200

    Version 1.0.3

---
+++ UnitTestsSources/UnitTestsMain.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+
+#include "../Core/Toolbox.h"
+#include "../Core/SQLite/Connection.h"
+#include "../Core/OrthancException.h"
+#include "../Core/DicomFormat/DicomTag.h"
+
+#include <sqlite3.h>
+
+using namespace OrthancPlugins;
+
+
+TEST(OrthancPluginRestApiGet, Basic){
+
+}
+
+
+
